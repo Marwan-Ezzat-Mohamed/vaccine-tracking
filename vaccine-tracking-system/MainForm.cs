443d840cf9d887eeb2206d6ec44ab0a9324ab27b
Marwan-Ezzat-Mohamed <marwan.ezzat.badawi@gmail.com>
2021-05-26 00:43:11 +0200

    intial commit

--- vaccine-tracking-system/MainForm.cs
+++ vaccine-tracking-system/MainForm.cs
@@ -11,68 +11,68 @@ using System.Windows.Forms;
 namespace vaccine_tracking_system
 {
     public partial class MainForm : Form
-    {
-        void setPercentageOfWhoAppliedForVaccination()
-        {
-            double numberOfWhoAppliedForVaccination = 0;
-            foreach (User user in Data.users)
-            {
-                if (user.waitingList || user.isVaccinated)
-                {
-                    numberOfWhoAppliedForVaccination++;
-                }
-            }
+    { 
+    //    void setPercentageOfWhoAppliedForVaccination()
+    //    {
+    //        double numberOfWhoAppliedForVaccination = 0;
+    //        foreach (User user in Data.users)
+    //        {
+    //            if (user.waitingList || user.isVaccinated)
+    //            {
+    //                numberOfWhoAppliedForVaccination++;
+    //            }
+    //        }
 
-            //percentageOfWhoAppliedBar..Value = (numberOfUnvaccinate / users.size()) * 100;
+    //        //percentageOfWhoAppliedBar..Value = (numberOfUnvaccinate / users.size()) * 100;
 
-        }
-
-
-
-        void setPercentageOfUnvaccinated()
-        {
-            double numberOfUnvaccinate = 0;
-
-            foreach (User user in Data.users)
-            {
-                if (!user.isVaccinated)
-                {
-                    numberOfUnvaccinate++;
-                }
-            }
-            //percentageOfUnvaccinatedBar->Value = (numberOfUnvaccinate / users.size()) * 100;
-
-        }
-
-        void setPercentageOfWhoGotAtleastOneDosebar()
-        {
-            double numberOfOneDose = 0;
-            foreach (User user in Data.users)
-            {
-                if (user.firstDose)
-                {
-                    numberOfOneDose++;
-                }
-            }
-            //percentageOfWhoGotAtleastOneDosebar->Value = (numberOfOneDose / users.size()) * 100;
-
-        }
+    //    }
 
 
 
-        void setPercentageOfWhoGotFullyVaccinated()
-        {
-            double numberOfOneDose = 0;
-            foreach (User user in Data.users)
-            {
-                if (user.secondDose || user.isVaccinated)
-                {
-                    numberOfOneDose++;
-                }
-            }
-            //percentageOfWhoGotFullyVaccinatedBar->Value = (numberOfOneDose / users.size()) * 100;
+    //    void setPercentageOfUnvaccinated()
+    //    {
+    //        double numberOfUnvaccinate = 0;
 
-        }
+    //        foreach (User user in Data.users)
+    //        {
+    //            if (!user.isVaccinated)
+    //            {
+    //                numberOfUnvaccinate++;
+    //            }
+    //        }
+    //        //percentageOfUnvaccinatedBar->Value = (numberOfUnvaccinate / users.size()) * 100;
+
+    //    }
+
+    //    void setPercentageOfWhoGotAtleastOneDosebar()
+    //    {
+    //        double numberOfOneDose = 0;
+    //        foreach (User user in Data.users)
+    //        {
+    //            if (user.firstDose)
+    //            {
+    //                numberOfOneDose++;
+    //            }
+    //        }
+    //        //percentageOfWhoGotAtleastOneDosebar->Value = (numberOfOneDose / users.size()) * 100;
+
+    //    }
+
+
+
+    //    void setPercentageOfWhoGotFullyVaccinated()
+    //    {
+    //        double numberOfOneDose = 0;
+    //        foreach (User user in Data.users)
+    //        {
+    //            if (user.secondDose || user.isVaccinated)
+    //            {
+    //                numberOfOneDose++;
+    //            }
+    //        }
+    //        //percentageOfWhoGotFullyVaccinatedBar->Value = (numberOfOneDose / users.size()) * 100;
+
+    //    }
 
 
         public MainForm()
